3a0fb94bf63ed23bf32009d4ccce7c3a80f58af8
Brooks Patton <brooks@brookzerker.com>
2016-11-27 21:55:12 -0700

    the ball bounces off player 1s paddle

--- main.lua
+++ main.lua
@@ -22,14 +22,47 @@ function love.load()
     player2 = 0,
     font = love.graphics.newFont(30)
   }
+
+  paddle1 = {
+    x = 10,
+    y = 280,
+    height = 40,
+    width = 10,
+    direction = nil,
+    speed = 200
+  }
+
+  paddle2 = {
+    x = 780,
+    y = 280,
+    height = 40,
+    width = 10
+  }
 end
 
 function love.update(dt)
   ball.x = ball.x + ball.speedX * dt
   ball.y = ball.y + ball.speedY * dt
 
-  if ball.y + ball.height > 600 or ball.y < 0 then
+  if ball.y + ball.height > 600 then
     ball.speedY = ball.speedY * -1
+    ball.y = 600 - ball.height
+  end
+
+  if ball.y < 0 then
+    ball.speedY = ball.speedY * -1
+    ball.y = 0
+  end
+
+  if ball.x < paddle1.x + paddle1.width and ball.y > paddle1.y and ball.y < paddle1.y + paddle1.height then
+    ball.speedX = ball.speedX * -1
+    ball.x = paddle1.x + paddle1.width
+  end
+
+  if paddle1.direction == 'up' and paddle1.y > 0 then
+    paddle1.y = paddle1.y - paddle1.speed * dt
+  elseif paddle1.direction == 'down' and paddle1.y + paddle1.height < 600 then
+    paddle1.y = paddle1.y + paddle1.speed * dt
   end
 end
 
@@ -37,8 +70,25 @@ function love.draw()
   love.graphics.setColor(unpack(ball.color))
   love.graphics.rectangle('fill', ball.x, ball.y, ball.width, ball.height)
 
+  love.graphics.rectangle('fill', paddle1.x, paddle1.y, paddle1.width, paddle1.height)
+  love.graphics.rectangle('fill', paddle2.x, paddle2.y, paddle2.width, paddle2.height)
+
   drawNet()
   drawScores()
+end
+
+function love.keypressed(key, scancode)
+  if scancode == 'w' then
+    paddle1.direction = 'up'
+  elseif scancode == 's' then
+    paddle1.direction = 'down'
+  end
+end
+
+function love.keyreleased(key, scancode)
+  if scancode == 'w' or scancode == 's' then
+    paddle1.direction = nil
+  end
 end
 
 function drawNet()
